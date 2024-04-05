9204fb658e1d35e26b8494d3e3fb71ee40050f57
josecontilde <116119633+josecontilde@users.noreply.github.com>
2024-04-04 21:23:09 -0500

    commit inicial

---
+++ resources/script.js
@@ -0,0 +1,4 @@
+document.getElementById("botonAbrir").addEventListener("click", function () {
+    document.querySelector(".content").classList.toggle("mover");
+    document.querySelector(".content.pagina").classList.toggle("mostrar");
+});
